99daee599b49c1d7f3a32e8576dd5890973b4da5
ananya <ananyaaa3080@gmail.com>
2026-08-17 15:25:21 +0530

    Time: 0 ms (100%), Space: 9.8 MB (94.31%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int j= s.size()-1;
+        int i=0;
+        while(i<j){
+            if(isalnum(s[i]) && isalnum(s[j])){
+                if(tolower(s[i])!=tolower(s[j])){
+                    return false;
+                }
+                i++;
+                j--;
+            }
+            else{
+                if(!isalnum(s[i])){
+                    i++;
+                }
+                else if (!isalnum(s[j])){
+                    j--;
+                }
+            }
+        }
+        return true;              
+    }
+};
