5477da579adb8a10fb8e99bce74ceda703d34051
ananya <ananyaaa3080@gmail.com>
2026-08-17 14:56:36 +0530

    Time: 0 ms (100%), Space: 27.3 MB (15.98%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int j=s.size()-1;
+        for(int i=0; i<s.size(); i++){
+            if(i>j){
+                break;
+            }
+            swap(s[i], s[j]);
+            j--;
+        }       
+    }
+};
